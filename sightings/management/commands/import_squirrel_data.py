e6ec01d306ccfa9473020515612b8b0b1a3fa133
Jennifer Huang <dh2987@columbia.edu>
2019-12-06 22:44:44 +0000

    Add models, migrate, and register superuser/app

--- sightings/management/commands/import_squirrel_data.py
+++ sightings/management/commands/import_squirrel_data.py
@@ -3,15 +3,15 @@ from django.core.management.base import BaseCommand
 from sightings.models import Squirrel
 from datetime import datetime
 
-class Command(BaseCommand): 
-    def add_arguments(self, parser): 
+class Command(BaseCommand):
+    def add_arguments(self, parser):
         parser.add_argument('csv_file')
 
-    def handle(self, *args, **options): 
-        with open(options['csv_file']) as fp: 
+    def handle(self, *args, **options):
+        with open(options['csv_file']) as fp:
             reader = csv.DictReader(fp)
-            data = list(reader) 
-        
+            data = list(reader)
+
         for item in data:
             L = [
             item['Running'],
@@ -34,29 +34,29 @@ class Command(BaseCommand):
                 elif L[i] in ('TRUE', 'true'):
                     L[i] = True
 
-            s = Squirrels( 
-                Latitude=item['Y'], 
-                Longitude=item['X'], 
-                Unique_Squirrel_ID=item['Unique Squirrel ID'], 
-                Shift=item['Shift'], 
+            s = Squirrel(
+                Latitude=item['Y'],
+                Longitude=item['X'],
+                Unique_Squirrel_ID=item['Unique Squirrel ID'],
+                Shift=item['Shift'],
                 Date=datetime.strptime(item['Date'], '%m%d%Y'),
-                Age=item['Age'], 
-                Primary_Fur_Color=item['Primary Fur Color'], 
+                Age=item['Age'],
+                Primary_Fur_Color=item['Primary Fur Color'],
                 Location=item['Location'],
-                Specific_Location=item['Specific Location'], 
-                Running=L[0], 
+                Specific_Location=item['Specific Location'],
+                Running=L[0],
                 Chasing=L[1],
-                Climbing=L[2], 
+                Climbing=L[2],
                 Eating=L[3],
-                Foraging=L[4], 
-                Other_Activities=item['Other Activities'], 
+                Foraging=L[4],
+                Other_Activities=item['Other Activities'],
                 Kuks=L[5],
-                Quaas=L[6], 
+                Quaas=L[6],
                 Moans=L[7],
-                Tail_flags=L[8], 
-                Tail_twitches=L[9], 
+                Tail_flags=L[8],
+                Tail_twitches=L[9],
                 Approaches=L[10],
-                Indifferent=L[11], 
+                Indifferent=L[11],
                 Runs_from=L[12],
             )
-            s.save()
+            s.save() 
--- sightings/models.py
+++ sightings/models.py
@@ -1,3 +1,120 @@
 from django.db import models
+from django.utils.translation import gettext as _
 
-# Create your models here.
+class Squirrel(models.Model):
+    AM = 'AM'
+    PM = 'PM'
+    Shift_Choices = (
+            (AM, 'am'),
+            (PM, 'pm'),
+     )
+    # Add characteristics of Squirrel
+
+    Latitude = models.FloatField(
+            help_text=_('Latitude coordinate for squirrel sighting point'),
+     )
+    
+    Longitude = models.FloatField(
+            help_text=_('Longitude coordinate for squirrel sighting point'),
+     )
+
+    Unique_Squirrel_ID = models.CharField(
+            help_text=_('Identification tag for each squirrel sightings'),
+            max_length=30,
+            primary_key=True,
+     )
+
+    Shift = models.CharField(
+            help_text=_('Whether or not the sighting session occurred in the morning or late afternoon'),
+            max_length=2,
+            choices=Shift_Choices,
+     )
+
+    Date = models.DateTimeField(
+            help_text=_('Concatenation of the sighting session day and month.'),
+     )
+
+    Age = models.CharField(
+            help_text=_('Value is either Adult or Juvenile.'),
+            max_length=20,
+            blank=True,
+            null=True,
+     )
+
+    Primary_Fur_Color = models.CharField(
+            help_text=_('Value is either Gray, Cinnamon or Black.'),
+            max_length=20,
+            blank=True,
+            null=True,
+     )
+
+    Location = models.CharField(
+            help_text=_('Value is either Ground Plane or Above Ground. Sighters were instructed to indicate the location of where the squirrel was when first sighted.'),
+            max_length=30,
+            blank=True,
+            null=True,
+     )
+    Specific_Location = models.CharField(
+            help_text=_('Sighters occasionally added commentary on the squirrel location. These notes are provided here.'),
+            max_length=30,
+            blank=True,
+            null=True,
+     )
+
+    Running = models.BooleanField(
+            help_text=_('Squirrel was seen running.'),
+     )
+
+    Chasing = models.BooleanField(
+            help_text=_('Squirrel was seen chasing another squirrel.'),
+     )
+
+    Climbing = models.BooleanField(
+            help_text=_('Squirrel was seen climbing a tree or other environmental landmark.'),
+     )
+
+    Eating = models.BooleanField(
+            help_text=_('Squirrel was seen eating.'),
+     )
+
+    Foraging = models.BooleanField(
+            help_text=_('Squirrel was seen foraging for food.'),
+     )
+    Other_Activities = models.CharField(
+            max_length=30,
+            blank=True,
+            null=True,
+     )
+    Kuks = models.BooleanField(
+            help_text=_('Squirrel was heard kukking, a chirpy vocal communication used for a variety of reasons.'),
+     )
+  
+    Quaas = models.BooleanField(
+            help_text=_('Squirrel was heard quaaing, an elongated vocal communication which can indicate the presence of a ground predator such as a dog.'),
+     )
+    
+    Moans = models.BooleanField(
+            help_text=_('Squirrel was heard moaning, a high-pitched vocal communication which can indicate the presence of an air predator such as a hawk.'),
+     )
+    
+    Tail_flags = models.BooleanField(
+            help_text=_("Squirrel was seen flagging its tail. Flagging is a whipping motion used to exaggerate squirrel's size and confuse rivals or predators. Looks as if the squirrel is scribbling with tail into the air."),
+     )
+    
+    Tail_twitches = models.BooleanField(
+            help_text=_('Squirrel was seen twitching its tail. Looks like a wave running through the tail, like a breakdancer doing the arm wave. Often used to communicate interest, curiosity.'),
+     )
+
+    Approaches = models.BooleanField(
+            help_text=_('Squirrel was seen approaching human, seeking food.'),
+     )
+    
+    Indifferent = models.BooleanField(
+            help_text=_('Squirrel was indifferent to human presence.'),
+     )
+    
+    Runs_from = models.BooleanField(
+            help_text=_('Squirrel was seen running from humans, seeing them as a threat.'),
+     )
+
+
